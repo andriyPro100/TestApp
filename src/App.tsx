642a863338522d43466aa43967347963a15a31a6
andriyPro100 <andriy.anatolich@gmail.com>
2021-02-23 17:10:15 +0200

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="container mx-auto my-10">
-        <h1>React start</h1>
-    </div>
-  );
+    const [openModal, setOpenModal] = useState(false)
+    return (
+        <div className="container mx-auto my-10">
+            <h1>Modal window</h1>
+            <Button text="Add client" color="bg-blue-500"/>
+            <h1>Table</h1>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export interface ButtonProps {
+    text: string
+    color: string
+}
+
+export default function Button ({text, color}:ButtonProps) {
+    return (
+        <button className={color + " m-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"}
+                >
+            {text}</button>
+    )
+}
